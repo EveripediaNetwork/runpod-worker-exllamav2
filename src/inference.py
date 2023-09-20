f484c79feb72bf1fdf90fc003c222f4b2b5f577b
Srujan Gurram <srujangs8@gmail.com>
2023-09-21 02:51:05 +0530

    Remove disallowing EOS tokens in the generation settings of the predictor

--- src/inference.py
+++ src/inference.py
@@ -40,7 +40,7 @@ class Predictor:
         self.model.load()
         self.cache = ExLlamaV2Cache(self.model)
         self.settings = ExLlamaV2Sampler.Settings()
-        self.settings.disallow_tokens(self.tokenizer, [self.tokenizer.eos_token_id])
+        # self.settings.disallow_tokens(self.tokenizer, [self.tokenizer.eos_token_id])
 
     def predict(self, settings):
         ### Set the generation settings
