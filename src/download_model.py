006dc24786ea6f5c569e6f90fb4364c7f248dc53
Srujan Gurram <srujangs8@gmail.com>
2023-10-16 14:47:51 +0530

    Added logic to accept loras

--- src/download_model.py
+++ src/download_model.py
@@ -3,25 +3,23 @@ from huggingface_hub import snapshot_download
 
 # Get the hugging face token
 HUGGING_FACE_HUB_TOKEN = os.environ.get("HUGGING_FACE_HUB_TOKEN", None)
-MODEL_NAME = os.environ.get("MODEL_NAME")
-MODEL_REVISION = os.environ.get("MODEL_REVISION", "main")
 MODEL_BASE_PATH = os.environ.get("MODEL_BASE_PATH", "/runpod-volume/")
 
 
-def download_model():
+def download_model(model_name: str, model_revision: str):
     # Download the model from hugging face
     download_kwargs = {}
 
     if HUGGING_FACE_HUB_TOKEN:
         download_kwargs["token"] = HUGGING_FACE_HUB_TOKEN
 
-    DOWNLOAD_PATH = f"{MODEL_BASE_PATH}{MODEL_NAME.split('/')[1]}"
+    DOWNLOAD_PATH = f"{MODEL_BASE_PATH}{model_name.split('/')[1]}"
 
     print(f"Downloading model to: {DOWNLOAD_PATH}")
 
     downloaded_path = snapshot_download(
-        repo_id=MODEL_NAME,
-        revision=MODEL_REVISION,
+        repo_id=model_name,
+        revision=model_revision,
         local_dir=DOWNLOAD_PATH,
         local_dir_use_symlinks=False,
         **download_kwargs,
